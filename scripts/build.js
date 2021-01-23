2a91d72c1f25aeef9347000a5c897bab39f484c2
shoonia <alexanderz@wix.com>
2021-01-23 14:39:15 +0200

    Type up

--- scripts/build.js
+++ scripts/build.js
@@ -5,28 +5,30 @@ const posthtml = require('posthtml');
 const minifyClassnames = require('posthtml-minify-classnames');
 
 const paths = require('./paths');
-const config = require('../webpack.config');
+const webpackConfig = require('../webpack.config');
 
 const { readFile, writeFile } = promises;
 
-const build = () => {
+const build = (config) => {
   return new Promise((resolve, reject) => {
-    webpack(config).run((error, stats) => {
+    webpack(config, (error, stats) => {
       if (error) reject(error);
       else resolve(stats);
     });
   });
 };
 
+process.env.BABEL_ENV = process.env.BROWSERSLIST_ENV = process.env.NODE_ENV = 'production';
+
 (async () => {
   console.log('\nProduction build...\n');
 
   await emptyDir(paths.dist);
-  await build();
+  await build(webpackConfig(process.env));
 
   const minify = minifyClassnames({ filter: /^#/ });
-  const text = await readFile(paths.distHtml, { encoding: 'utf8' });
-  const { html } = await posthtml().use(minify).process(text);
+  const text = await readFile(paths.distHtml, 'utf8');
+  const { html } = await posthtml(minify).process(text);
 
   await writeFile(paths.distHtml, html);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,3 @@
-const nodeEnv = process.env.BABEL_ENV = process.env.BROWSERSLIST_ENV = process.env.NODE_ENV;
-const isDev = nodeEnv === 'development';
-const isProd = nodeEnv === 'production';
-
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -12,164 +8,168 @@ const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').def
 const { homepage } = require('./package.json');
 const paths = require('./scripts/paths');
 
-module.exports = {
-  mode: nodeEnv,
-  bail: isProd,
-  devtool: isDev && 'cheap-module-source-map',
-  entry: paths.indexJs,
-  output: {
-    path: isProd ? paths.dist : undefined,
-    pathinfo: isDev,
-    filename: '[name].[contenthash:4].js',
-    publicPath: isProd ? homepage : '',
-  },
-  optimization: {
-    minimize: isProd,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          ecma: 2020,
-          module: true,
-          toplevel: true,
-          parse: {
-            ecma: 2018,
-          },
-          compress: {
-            ecma: 2018,
+module.exports = ({ NODE_ENV: nodeEnv }) => {
+  const isDev = nodeEnv === 'development';
+  const isProd = nodeEnv === 'production';
+
+  return {
+    mode: nodeEnv,
+    bail: isProd,
+    devtool: isDev && 'cheap-module-source-map',
+    entry: paths.indexJs,
+    output: {
+      path: isProd ? paths.dist : undefined,
+      pathinfo: isDev,
+      filename: '[name].[contenthash:4].js',
+      publicPath: isProd ? homepage : '',
+    },
+    optimization: {
+      minimize: isProd,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: 2020,
             module: true,
-            comparisons: false,
-            inline: 2,
-            drop_console: false,
-            passes: 5,
             toplevel: true,
-            pure_getters: true,
+            parse: {
+              ecma: 2018,
+            },
+            compress: {
+              ecma: 2018,
+              module: true,
+              comparisons: false,
+              inline: 2,
+              drop_console: false,
+              passes: 5,
+              toplevel: true,
+              pure_getters: true,
+            },
+            output: {
+              ecma: 2018,
+              comments: false,
+            },
           },
-          output: {
-            ecma: 2018,
-            comments: false,
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            map: false,
           },
-        },
-      }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: {
-          map: false,
-        },
-        cssProcessorPluginOptions: {
-          preset: [
-            'default',
-            {
-              discardComments: {
-                removeAll: true,
-                removeAllButFirst: true,
+          cssProcessorPluginOptions: {
+            preset: [
+              'default',
+              {
+                discardComments: {
+                  removeAll: true,
+                  removeAllButFirst: true,
+                },
               },
+            ],
+          },
+        }),
+      ],
+    },
+    resolve: {
+      modules: [
+        'node_modules',
+        paths.nodeModules,
+      ],
+      extensions: [
+        '.js',
+      ],
+    },
+    module: {
+      strictExportPresence: true,
+      rules: [
+        { parser: { requireEnsure: false } },
+        {
+          oneOf: [
+            {
+              test: /\.js$/,
+              include: paths.src,
+              loader: require.resolve('babel-loader'),
+              options: {
+                cacheDirectory: true,
+                cacheCompression: false,
+                compact: isProd,
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      loose: true,
+                      browserslistEnv: nodeEnv,
+                      configPath: paths.appDirectory,
+                      useBuiltIns: 'entry',
+                    },
+                  ],
+                ],
+              },
+            },
+            {
+              test: /\.css$/,
+              use: [
+                isDev && require.resolve('style-loader'),
+                isProd && MiniCssExtractPlugin.loader,
+                {
+                  loader: require.resolve('css-loader'),
+                  options: {
+                    importLoaders: 1,
+                    sourceMap: isDev,
+                  },
+                },
+                {
+                  loader: require.resolve('postcss-loader'),
+                  options: {
+                    sourceMap: isDev,
+                    postcssOptions: {
+                      plugins: [
+                        isProd && require('autoprefixer'),
+                        require('postcss-input-range'),
+                      ].filter(Boolean),
+                    },
+                  },
+                },
+              ].filter(Boolean),
             },
           ],
         },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        inject: 'head',
+        template: paths.indexHtml,
+        scriptLoading: 'defer',
+        favicon: paths.favicon,
+        minify: isProd && {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+        },
+        templateParameters: {
+          homepage,
+          isProd,
+        },
       }),
-    ],
-  },
-  resolve: {
-    modules: [
-      'node_modules',
-      paths.nodeModules,
-    ],
-    extensions: [
-      '.js',
-    ],
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      { parser: { requireEnsure: false } },
-      {
-        oneOf: [
-          {
-            test: /\.js$/,
-            include: paths.src,
-            loader: require.resolve('babel-loader'),
-            options: {
-              cacheDirectory: true,
-              cacheCompression: false,
-              compact: isProd,
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    loose: true,
-                    browserslistEnv: nodeEnv,
-                    configPath: paths.appDirectory,
-                    useBuiltIns: 'entry',
-                  },
-                ],
-              ],
-            },
-          },
-          {
-            test: /\.css$/,
-            use: [
-              isDev && require.resolve('style-loader'),
-              isProd && MiniCssExtractPlugin.loader,
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                  sourceMap: isDev,
-                },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  sourceMap: isDev,
-                  postcssOptions: {
-                    plugins: [
-                      isProd && require('autoprefixer'),
-                      require('postcss-input-range'),
-                    ].filter(Boolean),
-                  },
-                },
-              },
-            ].filter(Boolean),
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: 'head',
-      template: paths.indexHtml,
-      scriptLoading: 'defer',
-      favicon: paths.favicon,
-      minify: isProd && {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
-      },
-      templateParameters: {
-        homepage,
-        isProd,
-      },
-    }),
-    new webpack.DefinePlugin({
-      'process.platform': JSON.stringify(process.platform),
-      'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-      'process.env.NODE_DEBUG': JSON.stringify(isDev),
-      'process.env': '{}',
-      'process': 'undefined',
-    }),
-    isProd && new MiniCssExtractPlugin(),
-    isProd && new HTMLInlineCSSWebpackPlugin(),
-    isDev && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.platform': JSON.stringify(process.platform),
+        'process.env.NODE_ENV': JSON.stringify(nodeEnv),
+        'process.env': '{}',
+        'process': 'undefined',
+      }),
+      isProd && new MiniCssExtractPlugin(),
+      isProd && new HTMLInlineCSSWebpackPlugin(),
+      isDev && new webpack.HotModuleReplacementPlugin(),
     // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ].filter(Boolean),
-  performance: false,
-  devServer: {
-    contentBase: paths.dist,
-    compress: true,
-    port: 3000
-  }
+    ].filter(Boolean),
+    performance: false,
+    devServer: {
+      contentBase: paths.dist,
+      compress: true,
+      port: 3000,
+    },
+  };
 };
