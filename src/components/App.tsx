a266152272b816623e83f4b40520cc2be8b4c97a
shoonia <alexanderz@wix.com>
2023-03-07 17:52:44 +0200

    Add page layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,19 @@
+import s from './App.css';
+
 export const App: FC = () => {
   return (
-    <main>
-      main
-    </main>
+    <div class={s.page}>
+      <header class={s.header}>
+        <a href="/" class={s.logo}>
+          1x1
+        </a>
+        <div class={s.menu}></div>
+      </header>
+      <aside class={s.toolbar}></aside>
+      <main class={s.main}>
+        <h1>Dashboard</h1>
+      </main>
+      <footer class={s.footer}></footer>
+    </div>
   );
 };
