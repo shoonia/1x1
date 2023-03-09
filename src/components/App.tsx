1d6277ee678c14d831ce3adaff386a1c3b26c45a
shoonia <alexanderz@wix.com>
2023-03-09 16:37:42 +0200

    Update layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,23 @@ export const App: FC = () => {
   return (
     <div class={s.page}>
       <header class={s.header}>
-        <a href="/" class={s.logo}>
-          1x1
+        <a href="/" class={s.left}>
+          1x1 Pixel PNG
         </a>
-        <div class={s.menu}></div>
+        <div class={s.right}></div>
       </header>
       <aside class={s.toolbar}></aside>
       <main class={s.main}>
-        <h1>Dashboard</h1>
+        <h1>
+          One pixel Base64 encoded transparent PNG generator
+        </h1>
       </main>
-      <footer class={s.footer}></footer>
+      <footer class={s.footer}>
+        <div class={s.left}>
+          1x1
+        </div>
+        <div class={s.right}></div>
+      </footer>
     </div>
   );
 };
