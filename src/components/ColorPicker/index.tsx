349ff123683b41fb353199b6b046fdd84d1bf89c
shoonia <alexanderz@wix.com>
2023-03-09 22:10:20 +0200

    Add color picker

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,9 +1,10 @@
+import s from './styles.css';
 import { Group } from '../Group';
 
 export const ColorPicker: FC = () => {
   return (
     <Group open title="Picker">
-      Color picker
+      <hex-alpha-color-picker class={s.picker}></hex-alpha-color-picker>
     </Group>
   );
 };
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'hex-color-picker': {
+      'hex-alpha-color-picker': {
         ref?: (node: import('vanilla-colorful').HexColorPicker) => void;
         class?: string;
       }
