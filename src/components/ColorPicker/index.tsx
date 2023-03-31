1f554a0292e2976661c5b2d4e9ff558a664defa1
shoonia <alexanderz@wix.com>
2023-03-31 16:21:24 +0300

    Update chunk

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,13 +1,9 @@
 import type { HexColorPicker } from 'vanilla-colorful';
+import 'vanilla-colorful/hex-alpha-color-picker.js';
 
 import s from './styles.css';
 import { Group } from '../Group';
 import { connect, dispatch } from '../../store';
-
-import(
-  /* webpackChunkName: "hex-alpha-color-picker" */
-  'vanilla-colorful/hex-alpha-color-picker.js'
-);
 
 export const ColorPicker: FC = () => {
   const isBigScreen = window.matchMedia('(min-width:700px)').matches;
