03569cc0b2b9d06d5d12d4772aa63b712a54ff38
shoonia <alexanderz@wix.com>
2023-12-09 23:38:34 +0200

    Improve link

--- src/components/SupportUkraine/index.tsx
+++ src/components/SupportUkraine/index.tsx
@@ -2,11 +2,11 @@ import s from './styles.css';
 
 export const SupportUkraine: JSX.FC = () => (
   <div class={s.box}>
-    <svg aria-label="flag of Ukraine" viewBox="0 0 3 2" width="2em">
-      <path fill="#005bbb" d="M0 0h3v1H0z" />
-      <path fill="#ffd500" d="M0 1h3v1H0z" />
-    </svg>
-    <a href="https://u24.gov.ua/">
+    <a class={s.link} href="https://u24.gov.ua/">
+      <svg aria-label="flag of Ukraine" viewBox="0 0 3 2" width="2em">
+        <path fill="#005bbb" d="M0 0h3v1H0z" />
+        <path fill="#ffd500" d="M0 1h3v1H0z" />
+      </svg>
       Support Ukraine
     </a>
   </div>
