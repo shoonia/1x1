ff0522972383a44be0b8d5467498907661b5e048
shoonia <alexanderz@wix.com>
2025-05-14 19:07:10 +0300

    Update link logic

--- src/components/PixelGIF/index.tsx
+++ src/components/PixelGIF/index.tsx
@@ -1,18 +1,16 @@
 import s from './styles.css';
 import { connect } from '../../store';
 
-const url = 'https://shoonia.github.io/pixel-gif/#';
-
 export const PixelGIF: JSX.FC = () => {
   const ready: JSX.Ref<HTMLAnchorElement> = (a) =>
     connect('hex', (state) => {
-      a.href = url + state.hex.slice(0, 6);
+      a.hash = state.hex.slice(0, 6);
     });
 
   return (
     <a
       ref={ready}
-      href={url}
+      href="https://shoonia.github.io/pixel-gif/"
       class={s.link}
       aria-label="One pixel Base64 encoded GIF generator"
     >
